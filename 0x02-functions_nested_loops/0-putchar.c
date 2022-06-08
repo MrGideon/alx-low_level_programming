5b1ae68bf495723ff453cc59af0f677a0769b7f2
Grayson Gideon <grayson.gideon@hotmail.com>
2022-06-08 04:46:36 -0700

    changes for betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
-int main (void)
+/**
+ *main - Entry Point
+ *main.h function included
+ * return:0 as success
+ */
+int main(void)
 {
-_putchar(_);
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('/n');
+int _putchar(_);
+int _putchar('p');
+int _putchar('u');
+int _putchar('t');
+int _putchar('c');
+int _putchar('h');
+int _putchar('a');
+int _putchar('r');
+int _putchar('/n');
 return (0);
 
 }
