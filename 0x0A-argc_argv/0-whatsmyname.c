26357df7dd60cae34adcadb3062b9be8a9f5e242
Grayson Gideon <grayson.gideon@hotmail.com>
2022-06-24 02:54:30 -0700

    error reductionon task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
+
 /**
- * main - prints first argement 
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success
- */
+* main - print first argument
+* @argc: number of command line arguments.
+* @argv: array that contains the program command line arguments.
+* Return: 0 - success
+*/
 
-int main  (int argc char*argv[] ){
+int main(int argc, char *argv[])
+{
 
-	int i=0;
-	for (i=0; i<argc; i++){
-	printf(argv[i]);
-	printf("\n");
-	}
-	return(0);
+int i = 0;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n",argv[i]); 
+}
+return (0);
 }
