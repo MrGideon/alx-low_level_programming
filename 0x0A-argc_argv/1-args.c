a0c541183ea4ec1c536513608202ae272923f19a
Grayson Gideon <grayson.gideon@hotmail.com>
2022-06-24 05:04:02 -0700

    task 1 to 4

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+* main - print number of arguments
+* @argc: number of command line arguments.
+* @argv: array that contains the program command line arguments.
+* Return: 0 - success
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
+
