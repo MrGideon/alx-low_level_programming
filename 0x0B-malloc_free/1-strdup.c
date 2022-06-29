6406fa346e2f89394e9e1a59b4669863e6b9f040
Grayson Gideon <grayson.gideon@hotmail.com>
2022-06-29 11:39:39 -0700

    Chnages on task 2 ad 3, for betty stylling

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,9 +14,11 @@ unsigned int i, j;
 if (str == NULL)
 return (NULL);
 
-for (i = 0; str[i] != '\0'; i++);
+for (i = 0; str[i] != '\0'; i++)
+;
 
-strout = (char *)malloc(sizeof(char) * (i + 1));
+strout = (char *)malloc(sizeof(char) * (i + 1))
+;
 
 if (strout == NULL)
 return (NULL);
