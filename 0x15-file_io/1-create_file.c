37d53e7ab698e3233808d44388f3fb06957a5257
Grayson Gideon <grayson.gideon@hotmail.com>
2022-08-02 02:51:43 -0700

    tasks for file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,44 +1,34 @@
 #include "main.h"
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- *
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-size_t i;
-
-for (i = 0; str[i]; i++)
-return (i);
-}
-
-/**
- * create_file - creates a file.
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
- *
- * Return: 1 on success, -1 on failure
- */
+* create_file - Create a file with a specified name and content
+* @filename: Name of the file to create
+* @text_content: Content to read into the file
+*
+* Return: 1 on success, -1 on any failure
+*/
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-ssize_t len = 0;
+int file, len, closed;
+mode_t mode = S_IRUSR | S_IWUSR;
 
+len = 0;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-if (fd == -1)
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+if (file == -1)
 return (-1);
 if (text_content != NULL)
-len = write(fd, text_content, _strlen(text_content));
-close(fd);
-if (len == -1)
+{
+while (text_content[len] != '\0')
+	len++;
+write(file, text_content, len);
+}
+closed = close(file);
+if (closed == -1)
 return (-1);
 return (1);
 }
